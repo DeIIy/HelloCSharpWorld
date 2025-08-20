456fad0911fdd3bed537b827d0e6ecb9dd438bf5
delly <ytetlik6875@gmail.com>
2025-08-20 15:25:48 +0300

    Applying the Single Responsibility Principle to GCD calculation

--- HelloCSharpWorld/Program.cs
+++ HelloCSharpWorld/Program.cs
@@ -8,55 +8,116 @@ using System.Threading.Tasks;
 
 namespace HelloCSharpWorld
 {
-    internal class Program
+    public static class InputHandler
     {
-        static void Ebob(ref int ebobXY, int x, int y)
+        public static int IntInputHandler(string userNotice)
         {
-            int asalcarpan = 2;
-            while(x!=1 && y!=1)
+            int value;
+            while (true)
             {
-                if(x % asalcarpan == 0 && y % asalcarpan == 0) 
+                Console.Write(userNotice);
+                try
                 {
-                    x = x / asalcarpan; //sayıyı asal çarpana böldüm
-                    y = y / asalcarpan; //sayıyı asal çarpana böldüm
-                    ebobXY = ebobXY * asalcarpan; // Her iki sayıyı da böldüğü çarpıma dahil ettim
+                    value = Int32.Parse(Console.ReadLine());
+                    break;
                 }
-                else if(x % asalcarpan != 0 && y % asalcarpan == 0)
+                catch (FormatException)
                 {
-                    y = y / asalcarpan; // Sadece bu sayı bölünüyorsa böldüm
+                    Console.WriteLine("Lütfen sadece sayısal bir değer giriniz.");
                 }
-                else if (x % asalcarpan == 0 && y % asalcarpan != 0)
+                catch (OverflowException)
                 {
-                    x = x / asalcarpan; // Sadece bu sayı bölünüyorsa böldüm
+                    Console.WriteLine("Girdiğiniz sayı çok büyük veya küçük. Lütfen geçerli bir aralıkta sayı giriniz.");
                 }
-                else // Her iki sayı da tam bölünmüyorsa bir sonraki asal çarpana geçilecek
+                catch (ArgumentNullException)
                 {
-                    bool asalSayi = false; // Durum olumsuz!
-                    do
-                    {
-                        asalcarpan++; // Asal çarpanı bir artırdım yeni değer asal olmayabilir.
-                        for(int i = 2; i < asalcarpan; i++) // Döngü başlattım
-                        {
-                            if (asalcarpan % i == 0) break; // 1 veya kendisi haricindeki bir sayıya tam bölünmemeli
-                            else asalSayi = true; // Durum olumlu
-                        }
-                        if (asalSayi) break; // Döngüden çıktım.
-                    } while(true);
+                    Console.WriteLine("Herhangi bir giriş yapılmadı. Lütfen tekrar deneyiniz.");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Beklenmeyen bir hata oluştu. Lütfen tekrar deneyiniz.");
                 }
             }
+            return value;
         }
-        static void Main(string[] args)
+    }
+
+    internal class Program
+    {
+        static bool IsPrime(int number)
         {
-            Console.Title = "Ebob";
-            int ebob = 1;
-            Console.Write("Birinci Sayı:");
-            int bS = Int32.Parse(Console.ReadLine());
-            Console.Write("İkinci Sayı:");
-            int iS = Int32.Parse(Console.ReadLine());
-            Ebob(ref ebob, bS, iS);
-            Console.WriteLine("Girilen {0} ve {1} Sayılarının Ebobu: {2}",bS,iS,ebob);
-            Console.ReadLine();
+            if (number < 2) return false;
+            int sqrt = (int)Math.Sqrt(number);
+            for (int i = 2; i <= sqrt; i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
         }
-            
+
+        static int GetNextPrime(int currentPrime)
+        {
+            do
+            {
+                currentPrime++;
+            } while (!IsPrime(currentPrime));
+            return currentPrime;
+        }
+
+        static (int gcdResult, int firstValue, int secondValue) DivideIfDivisible(
+            int gcdResult, int factorCandidate, int firstValue, int secondValue)
+        {
+            if (firstValue % factorCandidate == 0 && secondValue % factorCandidate == 0)
+            {
+                gcdResult *= factorCandidate;
+
+                firstValue /= factorCandidate;
+                secondValue /= factorCandidate;
+            }
+            else if (firstValue % factorCandidate == 0)
+            {
+                firstValue /= factorCandidate;
+            }
+            else if (secondValue % factorCandidate == 0)
+            {
+                secondValue /= factorCandidate;
+            }
+
+            return (gcdResult, firstValue, secondValue);
+        }
+
+        static int CalculateGCD(int gcdResult, int firstValue, int secondValue)
+        {
+            int factorCandidate = 2;
+            while (firstValue != 1 && secondValue != 1)
+            {
+                if (firstValue % factorCandidate == 0 || secondValue % factorCandidate == 0)
+                {
+                    var result = DivideIfDivisible(gcdResult, factorCandidate, firstValue, secondValue);
+                    gcdResult = result.gcdResult;
+                    firstValue = result.firstValue;
+                    secondValue = result.secondValue;
+                }
+                else
+                {
+                    factorCandidate = GetNextPrime(factorCandidate);
+                }
+            }
+            return gcdResult;
+        }
+
+        static (int firstValue, int secondValue) GetNumberForGCD()
+        {
+            int firstValue = InputHandler.IntInputHandler("EBOB hesaplaması yapılacak ilk sayıyı giriniz: ");
+            int secondValue = InputHandler.IntInputHandler("EBOB hesaplaması yapılacak ikinci sayıyı giriniz: ");
+            return (firstValue, secondValue);
+        }
+
+        static void Main(string[] args)
+        {;
+            var (firstValue, secondValue) = GetNumberForGCD();
+            int gcdResult = CalculateGCD(1, firstValue, secondValue);
+            Console.WriteLine($"EBOB({firstValue}, {secondValue}) = {gcdResult}");
+        }
     }
 }
