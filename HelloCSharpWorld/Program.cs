e4a924b8cc74fc7869f45052354c6f19129c8b82
delly <ytetlik6875@gmail.com>
2025-09-01 06:22:49 +0300

    Add XML documentation for ICalculationRunner interface explaining its role in delegating main execution tasks

--- HelloCSharpWorld/Program.cs
+++ HelloCSharpWorld/Program.cs
@@ -121,6 +121,14 @@ namespace Temeller
     {
         void Run();
     }
+    /// <summary>
+    ///     TR: Asal çarpanlara ayırma algoritmasında kullanılmak üzere, bir
+    ///         sonraki asal çarpanın bulunması ve asal çarpan kontrolü
+    ///         işlevlerini içeren arayüz.
+    ///     EN: An interface in which the functionalities of finding the next
+    ///         prime factor and checking whether a number is a prime factor
+    ///         are included for use in the prime factorization algorithm.
+    /// </summary>
     public interface IPrimeProvider
     {
         bool IsPrime(int n);
