0680154d1c88ff7ada81ed8dc02b52de7005f5fa
delly <ytetlik6875@gmail.com>
2025-09-04 15:17:09 +0300

    add XML comments to ICalculatorFactory explaining GCD algorithm selection

--- HelloCSharpWorld/Interfaces/ICalculatorFactory.cs
+++ HelloCSharpWorld/Interfaces/ICalculatorFactory.cs
@@ -1,5 +1,11 @@
 ﻿namespace HelloCSharpWorld.Interfaces
 {
+    /// <summary>
+    ///     TR: En büyük ortak bölen bulma algoritmasının seçiminde 
+    ///         yararlanılan arayüz.
+    ///     EN: The interface used in selecting the greatest common divisor
+    ///         (GCD) algortihm.
+    /// </summary>
     public interface ICalculatorFactory
     {
         IGcdCalculator Create(string choice);
