b2ca3d6d1515fd2f796b8ed4c6be43d984653ed0
delly <ytetlik6875@gmail.com>
2025-09-04 15:13:49 +0300

    add XML comments to IOutputHandler describing console output methods

--- HelloCSharpWorld/Interfaces/IOutputHandler.cs
+++ HelloCSharpWorld/Interfaces/IOutputHandler.cs
@@ -1,5 +1,9 @@
 ﻿namespace HelloCSharpWorld.Interfaces
 {
+    /// <summary>
+    ///     TR: Konsol üzerinden kullanıcı bilgilendirmesi veren arayüz.
+    ///     EN: Interface that provides user information via console.
+    /// </summary>
     public interface IOutputHandler
     {
         void PrintSeparator();
