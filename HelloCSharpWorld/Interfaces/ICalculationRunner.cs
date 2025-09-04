1d789023353970450d6ce77a1ef2f600b7a46d72
delly <ytetlik6875@gmail.com>
2025-09-04 15:39:08 +0300

    add XML comments to ICalculationRunner explaining main function task aggregation

--- HelloCSharpWorld/Interfaces/ICalculationRunner.cs
+++ HelloCSharpWorld/Interfaces/ICalculationRunner.cs
@@ -1,5 +1,12 @@
 ﻿namespace HelloCSharpWorld.Interfaces
 {
+    /// <summary>
+    ///     TR: Ana fonksiyonun çalıştırma işlemlerinin bünyesinde toplanarak
+    ///         yükünün hafifletilmesinde kullanılan arayüz.
+    ///     EN: An interface in which the execution tasks of the main 
+    ///         function are aggregated, facilitating the reduction of its 
+    ///         workload.
+    /// </summary>
     public interface ICalculationRunner
     {
         void Run();
