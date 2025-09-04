8e5588ec7300fd3c03b5fb0264a242321f3a0af9
delly <ytetlik6875@gmail.com>
2025-09-04 15:41:49 +0300

    add XML comments to IValidator for GCD input validation

--- HelloCSharpWorld/Interfaces/IValidator.cs
+++ HelloCSharpWorld/Interfaces/IValidator.cs
@@ -1,5 +1,11 @@
 ﻿namespace HelloCSharpWorld.Interfaces
 {
+    /// <summary>
+    ///     TR: En büyük ortak bölen (EBOB) hesaplamalarında özel durumların
+    ///         ele alındığı arayüz.
+    ///     EN: An interface in which special cases are processed in the
+    ///         computation of the greatest common divisor (GCD).
+    /// </summary>
     public interface IValidator
     {
         (int? x, int? y) EnsureValidInputs(int x, int y);
