2e7bd2cebd031af02fffda264b6d54f58235ff7c
delly <ytetlik6875@gmail.com>
2025-09-04 14:31:48 +0300

    Add GcdCalculatorFactory to instantiate chosen GCD algorithm

---
+++ HelloCSharpWorld/Services/GcdCalculatorFactory.cs
@@ -0,0 +1,17 @@
+﻿using HelloCSharpWorld.Interfaces;
+
+namespace HelloCSharpWorld.Services
+{
+    public sealed class GcdCalculatorFactory : ICalculatorFactory
+    {
+        public IGcdCalculator Create(string choice)
+        {
+            switch (choice)
+            {
+                case "1": return new PrimeFactorizationGcdCalculator();
+                case "2": return new EuclideanModuloGcdCalculator();
+                default: return new PrimeFactorizationGcdCalculator();
+            }
+        }
+    }
+}
