8fcb862159e6bf628ef053133b0fe3adb01eab59
delly <ytetlik6875@gmail.com>
2025-09-04 15:15:46 +0300

    add XML comments to IGcdCalculator describing GCD calculation methods

--- HelloCSharpWorld/Interfaces/IGcdCalculator.cs
+++ HelloCSharpWorld/Interfaces/IGcdCalculator.cs
@@ -1,5 +1,13 @@
 ﻿namespace HelloCSharpWorld.Interfaces
 {
+    /// <summary>
+    ///     TR: En büyük ortak bölenin iki farklı yöntem kullanılarak 
+    ///         hesaplandığı bir arayüz: Asal çarpanlara ayırma algoritması 
+    ///         veya Öklid algoritması.
+    ///     EN: An interface where the greatest common divisor (GCD) is 
+    ///         calculated using two different methods: Prime factorization
+    ///         algorithm or the Euclidean algorithm.
+    /// </summary>
     public interface IGcdCalculator
     {
         string Name { get; }
