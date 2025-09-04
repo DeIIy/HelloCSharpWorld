07ac34987ae355b17ac44d6dc47296e455c5e417
delly <ytetlik6875@gmail.com>
2025-09-04 07:48:26 +0300

    Add ConsoleOutputHandler to handle console output and display messages

---
+++ HelloCSharpWorld/UI/ConsoleOutputHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using HelloCSharpWorld.Interfaces;
+
+namespace HelloCSharpWorld.UI
+{
+    public sealed class ConsoleOutputHandler : IOutputHandler
+    {
+        public void PrintSeparator()
+        {
+            Console.WriteLine(Messages.Separator);
+        }
+
+        public void PrintIntroMessage()
+        {
+            Console.Clear();
+            PrintSeparator();
+            Console.WriteLine(Messages.AppTitle);
+            PrintSeparator();
+            Console.WriteLine(Messages.Intro);
+            PrintSeparator();
+            Console.WriteLine(Messages.Tip);
+            PrintSeparator();
+        }
+
+        public void PrintLine(string message)
+        {
+            Console.WriteLine(message ?? string.Empty);
+        }
+    }
+}
