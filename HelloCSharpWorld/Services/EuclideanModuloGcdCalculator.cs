f604b92382e9264534435df7cf48ba2bdc6af9d7
delly <ytetlik6875@gmail.com>
2025-09-04 14:25:44 +0300

    Add EuclideanModuloGcdCalculator for GCD calculation using modulo method

---
+++ HelloCSharpWorld/Services/EuclideanModuloGcdCalculator.cs
@@ -0,0 +1,36 @@
+﻿using HelloCSharpWorld.Core;
+using HelloCSharpWorld.Interfaces;
+
+namespace HelloCSharpWorld.Services
+{
+    public sealed class EuclideanModuloGcdCalculator : IGcdCalculator
+    {
+        public string Name { get { return "Euclidean Modulo GCD Calculator"; } }
+
+        public int CalculateGcd(int x, int y)
+        {
+            ITableBuilder builder = new EuclideanTableBuilder();
+            ITablePrinter printer = new EuclideanTablePrinter();
+
+            builder.CreateNew();
+
+            int step = 1;
+
+            while (y != 0)
+            {
+                int remainder = x % y;
+
+                var s = GcdStep.Start(step, x, y, y, $"{x} % {y} = {remainder}")
+                    .WithAfter(remainder, y, $"{x} % {y} = {remainder}", false, remainder);
+
+                builder.AddStep(s);
+
+                x = y;
+                y = remainder;
+                step++;
+            }
+            printer.Print(builder.GetSteps());
+            return x;
+        }
+    }
+}
