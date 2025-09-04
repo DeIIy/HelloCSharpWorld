b7377443764578081f85d3bdd4735ed198f0f25e
delly <ytetlik6875@gmail.com>
2025-09-04 15:45:23 +0300

    add XML comments to IErrorHandler for manual error handling

--- HelloCSharpWorld/Interfaces/IErrorHandler.cs
+++ HelloCSharpWorld/Interfaces/IErrorHandler.cs
@@ -2,6 +2,12 @@
 
 namespace HelloCSharpWorld.Interfaces
 {
+    /// <summary>
+    ///     TR: Manuel olarak hata yönetiminin yapıldığı ve kullanıcının 
+    ///         kırmızı hata mesajıyla bilgilendirildiği arayüz.
+    ///     EN: An interface in which error handling is performed manually
+    ///         and the user is informed via red error messages.
+    /// </summary>
     public interface IErrorHandler
     {
         void HandleError(Error error);
