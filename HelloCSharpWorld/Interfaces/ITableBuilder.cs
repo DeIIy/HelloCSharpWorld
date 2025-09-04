a492097470617dd11b6a837ba987fe137b372039
delly <ytetlik6875@gmail.com>
2025-09-04 07:25:25 +0300

    Add ITableBuilder interface to manage GCD calculation steps

---
+++ HelloCSharpWorld/Interfaces/ITableBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using HelloCSharpWorld.Core;
+
+namespace HelloCSharpWorld.Interfaces
+{
+    public interface ITableBuilder
+    {
+        void CreateNew();
+        void AddStep(GcdStep step);
+        IReadOnlyList<GcdStep> GetSteps();
+    }
+}
