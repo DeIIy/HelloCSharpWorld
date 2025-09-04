cb8cd8d22c60bfde676e64334d93a38937bc21dc
delly <ytetlik6875@gmail.com>
2025-09-04 15:47:00 +0300

    add XML comments to ITableBuilder for GCD step table management

--- HelloCSharpWorld/Interfaces/ITableBuilder.cs
+++ HelloCSharpWorld/Interfaces/ITableBuilder.cs
@@ -3,6 +3,13 @@ using HelloCSharpWorld.Core;
 
 namespace HelloCSharpWorld.Interfaces
 {
+    /// <summary>
+    ///     TR: GcdStep sınıfına EBOB hesaplaması verilerinin kaydedildiği,
+    ///         tablo haline getirildiği ve konsol ekranına yazıldığı arayüz.
+    ///     EN: An interface in which GcdStep class stores GCD calculation 
+    ///         data, organizes it into a table, and outputs the table to 
+    ///         the console
+    /// </summary>
     public interface ITableBuilder
     {
         void CreateNew();
