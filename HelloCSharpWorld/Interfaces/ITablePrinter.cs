2e5d7724d8acf4e231178e5e8b763697e168495d
delly <ytetlik6875@gmail.com>
2025-09-04 16:43:34 +0300

    add XML comments for table printing interface

--- HelloCSharpWorld/Interfaces/ITablePrinter.cs
+++ HelloCSharpWorld/Interfaces/ITablePrinter.cs
@@ -3,6 +3,10 @@ using HelloCSharpWorld.Core;
 
 namespace HelloCSharpWorld.Interfaces
 {
+    /// <summary>
+    ///     TR: EBOB hesaplama adımlarını tablo formatında yazdırmak için kullanılan arayüz.
+    ///     EN: Interface used to print GCD calculation steps in a table format.
+    /// </summary>
     public interface ITablePrinter
     {
         void Print(IReadOnlyList<GcdStep> steps);
