814e42859d89357b051254e47b20ccdd85190b78
delly <ytetlik6875@gmail.com>
2025-09-04 07:15:00 +0300

    Add IInputHandler interface for standardized user input handling

---
+++ HelloCSharpWorld/Interfaces/IInputHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloCSharpWorld.Interfaces
+{
+    public interface IInputHandler
+    {
+        int GetInteger(string message);
+        string GetChoice(string message, params string[] allowedChoices);
+        (int x, int y) GetNumberFromUser(string firstMessage, string secondMessage);
+    }
+}
