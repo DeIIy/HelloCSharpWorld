f721db3562d4ec89bf88226bd0be22f1644309d1
delly <ytetlik6875@gmail.com>
2025-09-04 06:59:34 +0300

    Add ValidationException class in Core for structured validation errors

---
+++ HelloCSharpWorld/Core/ValidationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloCSharpWorld.Core
+{
+    public sealed class ValidationException : Exception
+    {
+        public ErrorCode Code { get; }
+
+        public ValidationException(ErrorCode code, string message) : base(message)
+        {
+            Code = code;
+        }
+    }
+}
