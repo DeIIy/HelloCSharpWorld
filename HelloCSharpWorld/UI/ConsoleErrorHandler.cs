a9e8d26196eb33c63823239511a49de7a1e0016a
delly <ytetlik6875@gmail.com>
2025-09-04 16:24:19 +0300

    add XML comments to ConsoleErrorHandler for red error message display

--- HelloCSharpWorld/UI/ConsoleErrorHandler.cs
+++ HelloCSharpWorld/UI/ConsoleErrorHandler.cs
@@ -4,8 +4,20 @@ using HelloCSharpWorld.Interfaces;
 
 namespace HelloCSharpWorld.UI
 {
+    /// <summary>
+    ///     TR: Konsola kırmızı renkle hata mesajlarını yazdırma sorumluluğunu
+    ///         üstlenen sınıf.
+    ///     EN: Class responsible for displaying error messages in red on the
+    ///         console.
+    /// </summary>
     public class ConsoleErrorHandler : IErrorHandler
     {
+        /// <summary>
+        ///     TR: Verilen hatayı kırmızı renkte konsola yazdırır ve ardından
+        ///         rengi sıfırlar.
+        ///     EN: Prints the given error to the console in red and then resets
+        ///         the color.
+        /// </summary>
         public void HandleError(Error error)
         {
             var previous = Console.ForegroundColor;
