3ae9f42df455d64c574775af4ee8458a7c2b4720
delly <ytetlik6875@gmail.com>
2025-09-04 07:45:17 +0300

    Add ConsoleErrorHandler to handle and display errors in red

---
+++ HelloCSharpWorld/UI/ConsoleErrorHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HelloCSharpWorld.Core;
+using HelloCSharpWorld.Interfaces;
+
+namespace HelloCSharpWorld.UI
+{
+    public class ConsoleErrorHandler : IErrorHandler
+    {
+        public void HandleError(Error error)
+        {
+            var previous = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error != null ? error.ToString() : "[Error]: <null>");
+            Console.ForegroundColor = previous;
+        }
+    }
+}
