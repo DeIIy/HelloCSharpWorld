3141aef7caaf295d6a1d8f4c9740401d78ee595b
delly <ytetlik6875@gmail.com>
2025-09-04 14:52:51 +0300

    add XML comments to ErrorCode enum for clarity

--- HelloCSharpWorld/Core/ErrorCode.cs
+++ HelloCSharpWorld/Core/ErrorCode.cs
@@ -1,5 +1,14 @@
 ﻿namespace HelloCSharpWorld.Core
 {
+    /// <summary>
+    ///     TR: Uygulama boyunca kullanılacak: 'ArgumentOutOfRangeException,
+    ///         ArgumentNullException, FormatException, OverflowException, 
+    ///         NullReferenceExpection, Exception' hataları temsil eden enum
+    ///         sabitleri.
+    ///     EN: Exceptions to be used throughout the application: 'OverflowException,
+    ///         ArgumentOutOfRangeException, ArgumentNullException, FormatException,
+    ///         NullReferenceException, Exception' enumerated constants.
+    /// </summary>
     public enum ErrorCode
     {
         InvalidChoice,
