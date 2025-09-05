19b8a6c8b31e3bf82d4f23ae65a79691d25263ec
delly <ytetlik6875@gmail.com>
2025-09-05 07:50:33 +0300

    add unit tests for EuclideanModuloGcdCalculator

---
+++ HelloCSharpWorld.Tests/EuclideanCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using HelloCSharpWorld.Services;
+using Xunit;
+
+namespace HelloCSharpWorld.Tests
+{
+    public class EuclideanCalculatorTests
+    {
+        [Theory]
+        [InlineData(48, 18, 6)]
+        [InlineData(54, 24, 6)]
+        [InlineData(101, 103, 1)]
+        [InlineData(20, 5, 5)]
+        [InlineData(20, 5, 5)]
+        public void EuclideanAlgorithm_ComputeGcd(int a, int b, int expected)
+        {
+            var calc = new EuclideanModuloGcdCalculator();
+            var result = calc.CalculateGcd(a, b);
+            Assert.Equal(expected, result);
+        }
+    }
+}
