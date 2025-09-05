f9c53c665a18d97bf52270451d88f7d3ca78a5a3
delly <ytetlik6875@gmail.com>
2025-09-05 07:52:24 +0300

    add unit tests for PrimeFactorizationGcdCalculator

---
+++ HelloCSharpWorld.Tests/PrimeFactorCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using HelloCSharpWorld.Services;
+using Xunit;
+
+namespace HelloCSharpWorld.Tests
+{
+    public class PrimeFactorCalculatorTests
+    {
+        [Theory]
+        [InlineData(48, 18, 6)]
+        [InlineData(54, 24, 6)]
+        [InlineData(101, 103, 1)]
+        [InlineData(20, 5, 5)]
+        public void PrimeFactorization_ComputesGcd(int a, int b, int expected)
+        {
+            var calc = new PrimeFactorizationGcdCalculator();
+            var result = calc.CalculateGcd(a, b);
+            Assert.Equal(expected, result);
+        }
+    }
+}
