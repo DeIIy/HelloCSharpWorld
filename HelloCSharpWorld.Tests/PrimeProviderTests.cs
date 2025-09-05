9201d5b88e1277437e8a3128a2c87a816980542f
delly <ytetlik6875@gmail.com>
2025-09-05 07:53:57 +0300

    add unit tests for SimplePrimeProvider

---
+++ HelloCSharpWorld.Tests/PrimeProviderTests.cs
@@ -0,0 +1,29 @@
+﻿using HelloCSharpWorld.Services;
+using Xunit;
+
+namespace HelloCSharpWorld.Tests
+{
+    public class PrimeProviderTests
+    {
+        [Theory]
+        [InlineData(2, true)]
+        [InlineData(3, true)]
+        [InlineData(4, false)]
+        [InlineData(17, true)]
+        [InlineData(18, false)]
+        public void IsPrime_Works(int value, bool expected)
+        {
+            var provider = new SimplePrimeProvider();
+            var result = provider.IsPrime(value);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GetNextPrime_FindsNextCorrectly()
+        {
+            var provider = new SimplePrimeProvider();
+            var next = provider.GetNextPrime(3);
+            Assert.Equal(5, next);
+        }
+    }
+}
